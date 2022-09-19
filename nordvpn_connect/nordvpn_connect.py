717470647425632068f06fae238ed0acae4e2ffd
Tumbleweed072795 <98277469+Tumbleweed072795@users.noreply.github.com>
2022-09-19 09:02:31 -0500

    Update nordvpn_connect.py

--- nordvpn_connect/nordvpn_connect.py
+++ nordvpn_connect/nordvpn_connect.py
@@ -156,6 +156,7 @@ def rotate_VPN(parameters: dict):
     old_ip = current_ip
     connect_to_server(command, cwd_path, opsys, server_to_connect_to)
     
+    logger.info("Waiting for confirmation...")
     time.sleep(5) #give the connect_to_server function time to process.
 
     check_ip_changed(old_ip)
